9808c994f62500e6a4e8b61f2886c2060f2f2fcd
zoltanlovasz94 <zoltan.lovasz@isys.uni-stuttgart.de>
2024-08-22 15:26:47 +0200

    test some code

--- motor_pcb.py
+++ motor_pcb.py
@@ -1,3 +1,5 @@
+# Needs to be copied under C:\Users\%username\Documents\KiCad\8.0\scripting\plugins
+# Pcbnew -> tools/plugins
 import pcbnew
 import os
 
@@ -11,6 +13,9 @@ class SimplePlugin(pcbnew.ActionPlugin):
         
         self.board = pcbnew.GetBoard()
         
+        self.phases = 3
+        self.phases_per_layer = 2
+        
         self.phases = phases = {
             0: "B.Cu",
             1: "In1.Cu",
@@ -20,15 +25,31 @@ class SimplePlugin(pcbnew.ActionPlugin):
             5: "In5.Cu",
         }
         
-        self.phases = 3
-        self.phases_per_layer = 2
-        
         self.phase_to_layer = {
             0: ["B.Cu", "In1.Cu"],
             1: ["In2.Cu", "In3.Cu"],
             2: ["In4.Cu", "In5.Cu"]
         }
         
+        self.start_connector = "J1"
+        
+    def draw_base_tracks(self):
+        component = board.FindFootprintByReference(self.start_connector)
+        
+        # 2
+        pad = component.FindPadByNumber("2")
+        start_point = pad.GetPosition()
+        end_point = pcbnew.VECTOR2I(start_point.x, start_point.y - pcbnew.FromMM(10))
+        track = pcbnew.PCB_TRACK(board)
+        track.SetStart(start_point)
+        track.SetEnd(end_point)
+        track.SetLayer(pcbnew.In2_Cu)  
+        track.SetWidth(pcbnew.FromMM(2))  
+        
+        track.SetNetCode(pad.GetNet().GetNetCode())
+
+        board.Add(track)
+
     def draw_phase(self):
         pass
         
@@ -38,6 +59,9 @@ class SimplePlugin(pcbnew.ActionPlugin):
     
     def Run(self):     
         self.draw_motor()
+        pcbnew.Refresh()
+        
+SimplePlugin().register()
         
         
             
