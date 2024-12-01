3d437be5b4d250788edd01ed8aa59533ec562a73
zoltanlovasz94 <zoltan.lovasz@isys.uni-stuttgart.de>
2024-12-01 10:07:14 +0100

    incomplete u shapes for on half phase

--- motor_pcb.py
+++ motor_pcb.py
@@ -2,6 +2,9 @@ import pcbnew
 import os
 
 class AdvancedLinearMotor(pcbnew.ActionPlugin):
+    pole_width = pcbnew.FromMM(18.75) # The value in FromMM is in mm
+    number_of_tracks_per_pole = 10
+
     def defaults(self):
         self.name = "proba"
         self.category = "A descriptive category name"
@@ -15,10 +18,15 @@ class AdvancedLinearMotor(pcbnew.ActionPlugin):
         self.track_width = self.net_classes['Default'].GetTrackWidth()
         
         self.vertical_length = pcbnew.FromMM(63) # The value in FromMM is in mm
-        self.pole_horizontal_length = pcbnew.FromMM(37.5) #2*18.75 
+        self.pole_horizontal_length = 2*self.pole_width #2*18.75 
         
         self.start_track_x = pcbnew.FromMM(56.25)
         self.start_track_y = pcbnew.FromMM(102)
+    
+    def set_track_width(self):
+        track_width = int((1 / self.number_of_tracks_per_pole) * self.pole_width - self.clearance)
+        self.net_classes['Default'].SetTrackWidth(track_width)
+        self.track_width = self.net_classes['Default'].GetTrackWidth()
         
     def add_track(self, start_x, start_y, end_x, end_y, width, layer):
         start_point = pcbnew.VECTOR2I(start_x, start_y)
@@ -64,23 +72,22 @@ class AdvancedLinearMotor(pcbnew.ActionPlugin):
         )
         
     def Run(self):
-        # Parameters for the U-shaped tracks
-        start_x = 0          # Starting X coordinate of the first pole
-        start_y = 50         # Starting Y coordinate of the U-shape
-        pole_width = 18.75   # Width of one pole (distance between white lines)
-        pole_height = 40     # Height of the U-shape
-        num_tracks = 6       # Number of tracks per pole (parameter to adjust)
-        num_poles = 8        # Total number of poles
-        layer = pcbnew.B_Cu  # Layer to draw the tracks
+        self.set_track_width()
         
         for i in range(5):
-            offset_x = int((i+0.5) * (self.track_width + self.clearance))
-            offset_y = int(i * (self.track_width + self.clearance))
-            # Generate U-shaped tracks for all poles
-            self.create_u_shape(self.start_track_x, self.start_track_y, 
-                                self.pole_horizontal_length, self.vertical_length,
-                                offset_x, offset_y,
-                                self.track_width, pcbnew.B_Cu)
+            
+            
+            for j in range(self.number_of_tracks_per_pole):
+                offset_x = int((j+0.5) * (self.track_width + self.clearance))
+                offset_y = int(j * (self.track_width + self.clearance))
+                
+                # Generate U-shaped tracks for all poles
+                self.create_u_shape(self.start_track_x, self.start_track_y, 
+                                    self.pole_horizontal_length, self.vertical_length,
+                                    offset_x, offset_y,
+                                    self.track_width, pcbnew.B_Cu)
+            
+            self.start_track_x += self.pole_horizontal_length
                             
         #self.add_track(50, 70, 50, 300, 2, pcbnew.F_Cu)
         pcbnew.Refresh()
